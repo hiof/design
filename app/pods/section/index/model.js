39b6ac64cae7df57e73aef58c38598ab648bc694
Kenneth Dahlstrøm <kenneth@keda.no>
2015-09-08 10:57:29 +0200

    Changed the way pods work
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/pods/section/index/model.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  author: DS.attr('string'),
-  basename: DS.attr('string'),
-  date: DS.attr('string'),
-  preview: DS.attr('string')
-});
